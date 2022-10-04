ad75d5ee431b8df8b08f9756fb056f0944a6bb50
Burak Çakır <burak2043@gmail.com>
2022-10-04 11:35:48 +0300

    navigasyona baslandi

---
+++ navigasyon/src/pages/First.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function First() {
+    return (
+        <SafeAreaView>
+            <Text>Hello first !!</Text>
+        </SafeAreaView>
+    )
+}
+export default First
