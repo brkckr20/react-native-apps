454ed6675a6aa23f86a543480b935a4419d3cbfa
brkckr20 <burak2043@gmail.com>
2023-10-02 16:47:58 +0300

    sqlite started

--- harcamaTakip/src/screens/Home/index.tsx
+++ harcamaTakip/src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   View,
@@ -9,12 +9,65 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import DatePicker from 'react-native-date-picker';
+import SQLite from 'react-native-sqlite-storage';
 
 import {colors} from '../../colors';
+
+const db = SQLite.openDatabase(
+  {
+    location: 'default',
+    name: 'urunTakip1',
+  },
+  () => {
+    console.log('başarılı 3');
+  },
+  (err: any) => {
+    console.log(err);
+  },
+);
 
 export const Home = () => {
   const [date, setDate] = useState(new Date());
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'CREATE TABLE IF NOT EXIST urun (ID INTEGER PRIMARY KEY AUTOINCREMENT, TARIH DATE, MIKTAR NUMERIC, TUTAR REAL)',
+        [],
+        (tx, err) => {
+          console.log('tx', tx);
+          console.log('err', err);
+        },
+      );
+    });
+  }, []);
+
+  const kayitEkle = () => {
+    for (let i = 0; i < 20; i++) {
+      const tarih = new Date().toISOString().slice(0, 19).replace('T', ' ');
+      const miktar = 1;
+      db.transaction(tx => {
+        tx.executeSql(
+          'INSERT INTO urun (TARIH, MIKTAR) VALUES (?,?)',
+          [tarih, miktar],
+          (tx, result) => {
+            console.log(tx);
+            console.log(result);
+          },
+        );
+      });
+    }
+  };
+
+  const read = () => {
+    db.transaction(tx => {
+      tx.executeSql('select * from urun', [], (tx, res) => {
+        for (let i = 0; i < res.rows.length; i++) {}
+      });
+    });
+  };
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: colors.mavi, padding: 10}}>
       <StatusBar backgroundColor={colors.mavi} />
@@ -88,14 +141,14 @@ export const Home = () => {
           }}>
           <TouchableOpacity
             style={{flex: 1, backgroundColor: colors.lightIndigo, padding: 10}}
-            onPress={() => {}}>
+            onPress={kayitEkle}>
             <Text style={{color: colors.white, textAlign: 'center'}}>
               Kaydet
             </Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={{flex: 1, backgroundColor: colors.reptileGreen, padding: 10}}
-            onPress={() => {}}>
+            onPress={read}>
             <Text style={{color: colors.white, textAlign: 'center'}}>Yeni</Text>
           </TouchableOpacity>
         </View>
